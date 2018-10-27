d3cdff932f37bc6bb104a22957f2582191bfec34
PhelanM <phelanmccall@yahoo.com>
2018-10-27 00:50:01 -0700

    fixed timing, added delete buttons; TODO: login stuff

--- logic.js
+++ logic.js
@@ -1,23 +1,51 @@
 $(document).ready(function(){
 
-var trains = [{name:"transsiberian",
-               destination: "siberia",
-            firstDeparture: new Date("oct 31 2018 12:30"),
-            frequency: 25}];
+    var config = {
+        apiKey: "AIzaSyAwKKiPMVVnzZ2xADtp7hBjmP8CvtXCoOU",
+        authDomain: "anything-you-want-7a13a.firebaseapp.com",
+        databaseURL: "https://anything-you-want-7a13a.firebaseio.com",
+        projectId: "anything-you-want-7a13a",
+        storageBucket: "anything-you-want-7a13a.appspot.com",
+        messagingSenderId: "529923747414"
+      };
+      firebase.initializeApp(config);
+      
+      
+      // Assign the reference to the database to a variable named 'database'
+      // var database = ...
+      var database = firebase.database();
+      
 
+var trains = [];
+
+database.ref().on("value", function(snapshot){
+    if(snapshot.child("trains").exists && snapshot.val() != null){
+        trains = JSON.parse(snapshot.val().trains);
+        console.log(trains);
+        renderTrains();
+    }
+
+    else{
+        trains=[];
+    }
+},function(error){
+    console.log("I AM ERROR " + error.code);
+});
 
 function getNextArrival(start, now, freq ){
-
+    console.log("GOT " +  start + now + freq);
     if(freq < 1){
         return ["Never" , 0];
     }
 
     var post, na, ma;
-
+console.log(start.getHours()+":"+start.getMinutes()+" O'Clock");
     var startTime = 60 * start.getHours() + start.getMinutes();
     var nowTime = 60 * now.getHours() + now.getMinutes();
 
+    console.log("start: " + startTime + " now: " + nowTime);
     while(startTime <= nowTime){
+        
         startTime = startTime + freq;
     }
 
@@ -51,16 +79,18 @@ function getNextArrival(start, now, freq ){
 };
 
 function renderTrains(){
+  
     var dt = new Date();
     $("#trainList tbody").empty();
     for(let train in trains){
-
-        var trainData = $("<tr>");
+        console.log("rendering " + Object.keys(trains[train]) + trains[train].firstDeparture); 
+     
+        var trainData = $("<tr>").attr("data", trains[train].name);
         trainData.append($("<td>").text(trains[train].name));
         trainData.append($("<td>").text(trains[train].destination));
         trainData.append($("<td>").text(trains[train].frequency));
-
-        var arrival = getNextArrival(trains[train].firstDeparture, dt, parseInt(trains[train].frequency));
+       
+        var arrival = getNextArrival(new Date(trains[train].firstDeparture), dt, parseInt(trains[train].frequency));
         var nextArrival = arrival[0];
         var minutesAway = arrival[1];
 
@@ -68,6 +98,10 @@ function renderTrains(){
 
        
         trainData.append($("<td>").text(minutesAway));
+        var btn = $("<button>").text("DEL").addClass("delete btn btn-danger");
+       
+        trainData.append(btn);
+
         $("#trainList").append(trainData);
     };
 }
@@ -104,11 +138,32 @@ $("#submitTrainBtn").on("click", function(e){
     };
 
     trains.push(newTrain);
+
+    database.ref().set({
+        trains: JSON.stringify(trains)
+    });
     renderTrains();
     
 });
 
+$(document).on("click", ".delete", function(e){
+    e.preventDefault();
+    for(let t in trains){
+        if(trains[t].name == this.parentNode.attributes.data.nodeValue){
+            console.log("GOT EM");
+
+            trains.splice(t, 1);
+        }
+    }
+    database.ref().set({
+        trains: JSON.stringify(trains)
+    });
+    console.log(this.parentNode.attributes.data.nodeValue)
+    
+
+});
+
 renderTrains();
-setTimeout(renderTrains, 60000);
+setInterval(renderTrains, 1000);
 
 });
