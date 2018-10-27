592c2066cc7f5ca4e128d18e24fc88462c848af7
PhelanM <phelanmccall@yahoo.com>
2018-10-26 23:02:18 -0700

    Basic functionality achieved; TODO: add firebase

--- logic.js
+++ logic.js
@@ -2,52 +2,52 @@ $(document).ready(function(){
 
 var trains = [{name:"transsiberian",
                destination: "siberia",
-            firstDeparture: new Date("oct 31 2018 04:20"),
-            frequency: 1200}];
+            firstDeparture: new Date("oct 31 2018 12:30"),
+            frequency: 25}];
 
 
 function getNextArrival(start, now, freq ){
 
-    var post;
-    var startHours = start.getHours();
-    var startMin = start.getMinutes();
-    var nowHours = now.getHours();
-    var nowMin = now.getMinutes();
-    console.log(startHours < nowHours);
+    if(freq < 1){
+        return ["Never" , 0];
+    }
 
-    while(startHours <= nowHours){
-        startMin += freq;
-        while(startMin >= 59){
-            startHours++;
-            startMin = startMin - 60;
-        }
-        
-        console.log(startHours +":" + startMin);
-    
+    var post, na, ma;
+
+    var startTime = 60 * start.getHours() + start.getMinutes();
+    var nowTime = 60 * now.getHours() + now.getMinutes();
+
+    while(startTime <= nowTime){
+        startTime = startTime + freq;
     }
-    if(startMin >= 59){
-        startHours++;
-        startMin = startMin - 60;
-    }
+
   
-    if(startHours > 12){
-        startHours -= 12;
-        post = "PM";
-    }else{
-        post = "AM";
+    ma = startTime - nowTime;
+
+    var hour = Math.floor(startTime/60);
+    var min = startTime % 60;
+
+    while(hour > 24){
+        hour -= 24;
     }
 
-    var ma =  (60-nowMin)+ startMin;
+    if(hour > 12){
+        hour -=12;  
+        post =" PM";
+    }else{
+        post ="AM"
+    }
 
-    if(startMin < 10){
-        startMin = "0"+ startMin;
-    }   
+    if(min < 10){
+        min = "0" + min;
+    }
+    na = hour + ":" + min + post;
    
-    console.log("min away: " + ma);
-    var na = startHours +":"+ startMin + " " + post;
-    console.log("next arr: " +na);
 
     return [na, ma];
+    
+  
+
 };
 
 function renderTrains(){
@@ -60,10 +60,10 @@ function renderTrains(){
         trainData.append($("<td>").text(trains[train].destination));
         trainData.append($("<td>").text(trains[train].frequency));
 
-        var arrival = getNextArrival(trains[train].firstDeparture, dt, trains[train].frequency);
+        var arrival = getNextArrival(trains[train].firstDeparture, dt, parseInt(trains[train].frequency));
         var nextArrival = arrival[0];
         var minutesAway = arrival[1];
-        console.log(nextArrival);
+
         trainData.append($("<td>").text(nextArrival)); 
 
        
@@ -72,19 +72,39 @@ function renderTrains(){
     };
 }
 
-$("#submitTrainBtn").click(function(e){
+$("#submitTrainBtn").on("click", function(e){
     e.preventDefault();
 
     var dt = new Date();
+
+    var name = $("#trainName").val();
+    var dest =  $("#destination").val();
+    var time = [$("#hourInput").val(), $("#minInput").val()];
+    var first = new Date(  dt.getFullYear(), dt.getMonth(), dt.getDate() , time[0], time[1]);
+    var freq = $("#frequency").val();
     
+    if(name == ""){
+        name = "No name";
+    }
+    if(dest == ""){
+        dest = "No destination"
+    }
+    if(!first){
+        first = dt;
+    }
+    if(freq == NaN){
+        freq = 1;
+    }
+
     var newTrain = {
-        name: $("#trainName").val(),
-        destination: $("#destination").val(),
-        firstDeparture: new Date(  dt.getFullYear(), dt.getMonth(), dt.getDate() , $("#firstDeparture").val()),
-        frequency : $("#frequency").val()
+        name: name,
+        destination: dest,
+        firstDeparture: first,
+        frequency : freq
     };
-    console.log(newTrain.firstDeparture);
+
     trains.push(newTrain);
+    renderTrains();
     
 });
 
